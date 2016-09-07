0fa863d2559e73ae14b8bc91f27ecd76770ff39a
ming1012 <ming_1012@foxmail.com>
2016-09-07 15:58:59 +0800

    初始化项目，生成二维码

---
+++ 01-生成二维码/01-生成二维码/ViewController.m
@@ -0,0 +1,73 @@
+//
+//  ViewController.m
+//  01-生成二维码
+//
+//  Created by xiaomage on 15/8/19.
+//  Copyright (c) 2015年 小码哥. All rights reserved.
+//
+
+#import "ViewController.h"
+#import <CoreImage/CoreImage.h>
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    // 1.创建过滤器
+    CIFilter *filter = [CIFilter filterWithName:@"CIQRCodeGenerator"];
+    
+    // 2.恢复默认
+    [filter setDefaults];
+    
+    // 3.给过滤器添加数据(正则表达式/账号和密码)
+    NSString *dataString = @"http://www.520it.com";
+    NSData *data = [dataString dataUsingEncoding:NSUTF8StringEncoding];
+    [filter setValue:data forKeyPath:@"inputMessage"];
+    
+    // 4.获取输出的二维码
+    CIImage *outputImage = [filter outputImage];
+    
+    // 5.显示二维码
+    self.imageView.image = [self createNonInterpolatedUIImageFormCIImage:outputImage withSize:200];
+}
+
+/**
+ *  根据CIImage生成指定大小的UIImage
+ *
+ *  @param image CIImage
+ *  @param size  图片宽度
+ */
+- (UIImage *)createNonInterpolatedUIImageFormCIImage:(CIImage *)image withSize:(CGFloat) size
+{
+    CGRect extent = CGRectIntegral(image.extent);
+    CGFloat scale = MIN(size/CGRectGetWidth(extent), size/CGRectGetHeight(extent));
+    
+    // 1.创建bitmap;
+    size_t width = CGRectGetWidth(extent) * scale;
+    size_t height = CGRectGetHeight(extent) * scale;
+    CGColorSpaceRef cs = CGColorSpaceCreateDeviceGray();
+    CGContextRef bitmapRef = CGBitmapContextCreate(nil, width, height, 8, 0, cs, (CGBitmapInfo)kCGImageAlphaNone);
+    
+    CIContext *context = [CIContext contextWithOptions:nil];
+    CGImageRef bitmapImage = [context createCGImage:image fromRect:extent];
+    CGContextSetInterpolationQuality(bitmapRef, kCGInterpolationNone);
+    CGContextScaleCTM(bitmapRef, scale, scale);
+    CGContextDrawImage(bitmapRef, extent, bitmapImage);
+    
+    // 2.保存bitmap到图片
+    CGImageRef scaledImage = CGBitmapContextCreateImage(bitmapRef);
+    CGContextRelease(bitmapRef);
+    CGImageRelease(bitmapImage);
+    return [UIImage imageWithCGImage:scaledImage];
+}
+
+@end
